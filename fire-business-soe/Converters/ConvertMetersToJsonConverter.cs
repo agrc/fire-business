838a3b5f9eec4606980ff8113a6bd49313ae3d8e
steveoh <sgourley@utah.gov>
2017-08-24 12:22:04 -0600

    create converter for acres
    just in case

---
+++ fire-business-soe/Converters/ConvertMetersToJsonConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using fire_business_soe.Extensions;
+using Newtonsoft.Json;
+
+namespace fire_business_soe.Converters
+{
+    public class ConvertMetersToJsonConverter : JsonConverter
+    {
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof (double);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((double) value).InAcres());
+        }
+    }
+}
