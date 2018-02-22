065de8d2fd51811f303aae946c5eeafb2cbc4c3c
steveoh <sgourley@utah.gov>
2018-02-22 11:59:51 -0700

    update extent code

--- geoprocessing/FireImage/GenerateImage.py
+++ geoprocessing/FireImage/GenerateImage.py
@@ -6,52 +6,87 @@ Given a project id, width, and height this service returns a png map image
 showing the features for that project.
 '''
 
-from arcpy import env
-from arcpy import Extent
-from arcpy import GetParameterAsText
-from arcpy import mapping
-from arcpy import SetParameterAsText
-from math import isnan
-from os.path import dirname
-from os.path import join
+from os.path import dirname, join
 
-path_to_mxd = join(dirname(__file__), 'fire.mxd')
-image_path = join(env.scratchFolder, 'map_export.png')
+import arcpy
+
+path_to_mxd = join(dirname(__file__), 'FireAreas.mxd')
+image_path = join(arcpy.env.scratchFolder, 'map_export.png')
 
 
 def main(project_id, width, height):
-    mxd = mapping.MapDocument(path_to_mxd)
-    data_frame = mapping.ListDataFrames(mxd)[0]
-    feature_layers = [mapping.ListLayers(mxd)[0]]
-    xmin = None
-    ymin = None
-    xmax = None
-    ymax = None
+    mxd = arcpy.mapping.MapDocument(path_to_mxd)
+
+    data_frame = arcpy.mapping.ListDataFrames(mxd)[0]
+    layers = arcpy.mapping.ListLayers(mxd)
+
+    feature_layers = [layers[0], layers[1]]
+    where_clause = 'id={}'.format(project_id)
+    utm = arcpy.SpatialReference(26912)
+
+    bag = xmin = ymin = xmax = ymax = None
+    offset = 10
 
     for l in feature_layers:
-        l.definitionQuery = 'id = {}'.format(project_id)
-        extent = l.getExtent()
-        if xmin is None or isnan(xmin) or extent.XMin < xmin:
-            xmin = extent.XMin
-        if ymin is None or isnan(ymin) or extent.YMin < ymin:
-            ymin = extent.YMin
-        if xmax is None or isnan(xmax) or extent.XMax > xmax:
-            xmax = extent.XMax
-        if ymax is None or isnan(ymax) or extent.YMax > ymax:
-            ymax = extent.YMax
+        l.definitionQuery = where_clause
+
+        with arcpy.da.SearchCursor(l, 'SHAPE@', where_clause=where_clause) as cursor:
+            for shape, in cursor:
+                if shape is None:
+                    continue
+
+                if shape.spatialReference.factoryCode != 26912:
+                    shape = shape.projectAs(utm, 'NAD_1983_To_WGS_1984_5')
+
+                if shape.type == 'point':
+                    point = shape.centroid
+
+                    if point is None or point.X == 0:
+                        arcpy.AddMessage('Empty geometry found in {} id: {}'.format(l.name, project_id))
+                        continue
+
+                    shape = arcpy.Polygon(arcpy.Array([arcpy.Point(point.X - offset, point.Y - offset),
+                                                       arcpy.Point(point.X + offset, point.Y + offset),
+                                                       arcpy.Point(point.X - offset, point.Y + offset),
+                                                       arcpy.Point(point.X + offset, point.Y - offset)]),
+                                          utm)
+
+                if bag is None:
+                    bag = shape
+                    continue
+
+                bag = bag.union(shape)
 
     #: validate that features were found
-    if isnan(xmin):
-        raise Exception('No features found for project id: {}!'.format(project_id))
+    if bag is None:
+        arcpy.AddMessage('No features found for project id: {}'.format(project_id))
 
-    data_frame.extent = Extent(xmin, ymin, xmax, ymax)
+        return ''
 
-    mapping.ExportToPNG(mxd, image_path, data_frame=mxd.activeDataFrame, df_export_width=int(width), df_export_height=int(height))
+    extent = bag.extent
+
+    xmax = extent.XMax
+    xmin = extent.XMin
+    ymax = extent.YMax
+    ymin = extent.YMin
+
+    if xmax == xmin:
+        xmax = extent.XMax + offset
+        xmin = extent.XMin - offset
+        ymax = extent.YMax + offset
+        ymin = extent.YMin - offset
+
+    delta_x = (xmax - xmin) * .25
+    delta_y = (ymax - ymin) * .25
+
+    data_frame.extent = arcpy.Extent(xmin - delta_x, ymin - delta_y, xmax + delta_x, ymax + delta_y)
+
+    arcpy.mapping.ExportToPNG(mxd, image_path, data_frame=mxd.activeDataFrame, df_export_width=int(width), df_export_height=int(height))
 
     print(image_path)
     return image_path
 
 
 if __name__ == '__main__':
-    result = main(GetParameterAsText(0), GetParameterAsText(1), GetParameterAsText(2))
-    SetParameterAsText(3, result)
+    result = main(arcpy.GetParameterAsText(0), arcpy.GetParameterAsText(1), arcpy.GetParameterAsText(2))
+    arcpy.SetParameterAsText(3, result)
